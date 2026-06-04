86af7eff8d24d7022d7f64ed1d660f1dfe77a2fa
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-04 19:15:29 -0400

    modification

--- Q1.java
+++ Q1.java
@@ -9,6 +9,7 @@ public class Q1 {
     }
     public static void main (String[]args){
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
