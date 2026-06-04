736ce87c1899af6293aa56305542c6200e3fdbdd
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-04 19:14:31 -0400

    Average of given numbers

---
+++ Q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Q1 {
+    public static double countAverage (int a,int b,int c){
+        double sum = a + b + c;
+        double average = sum/3;
+
+        System.out.println(average);
+        return average;
+    }
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println("Average of given number : " + countAverage(a, b, c) );
+
+    }
+    
+}
