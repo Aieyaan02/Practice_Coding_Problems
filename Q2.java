bfb2949650855e36c14031823a14b7b31eb0df04
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-06 04:45:49 -0400

    practice

--- Q2.java
+++ Q2.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 public class Q2 {
-    public static int SumOdd (int n){
+    public static void SumOdd (int n){
+        int a = 0;
         for(int i = 1; i <= n; i++){
-            if (i % 2 == 1){
-
-                System.out.println();
-            }    
+            if (i % 2 != 0){
+                a = a + i;
+            } 
         }  
-        return SumOdd(n);
+        System.out.println(a);
+        
     }
     public static void main (String[]agrs){
-        
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int n = scan.nextInt();
+        SumOdd(n);
+        scan.close();
     }
     
 }
