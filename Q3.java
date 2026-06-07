54f4dfdb25c9bc53d206611369c05c86f05d07be
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-07 17:45:41 -0400

    Q3 function

--- Q3.java
+++ Q3.java
@@ -1,7 +1,13 @@
 public class Q3 {
-    public static int xyz (int a, int b){
-        if ()
+    public static int greaterNum (int a, int b){
+        if (a > b){
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+        return greaterNum(a, b);
         
     }
+    
     
 }
