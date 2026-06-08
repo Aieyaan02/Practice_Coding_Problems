8584820f1f87045a1935971550391a42ce0f61a3
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-08 15:53:27 -0400

    work

---
+++ Q4.java
@@ -0,0 +1,9 @@
+public class Q4 {
+    public static void radius (double r){
+        double area = Math.PI * r * r;
+        double circumference = 2 * Math.PI * r;
+        System.out.println("The area of the circle is: " + area);
+        System.out.println("The circumference of the circle is: " + circumference);
+    }
+    
+}
