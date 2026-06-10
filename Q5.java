f4be6c910e5cc84db2a1b81840b4d9f7e67ff8c4
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-10 15:50:39 -0400

    work

--- Q5.java
+++ Q5.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class Q5 {
-    public static void elegible (int vote){
-        if(age > 18){
+    public static void elegible (int age){
+        
+        if(age >= 18){
             System.out.println("Elegible to vote");
         } else {
             System.out.println("You are not elegible to vote right now");
@@ -9,6 +10,12 @@ public class Q5 {
 
     }
     public static void main (String[]args){
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("Hello Please enter your age : ");
+        int age = scan.nextInt();
+        elegible(age);
+        scan.close();
+
 
     }
 }
