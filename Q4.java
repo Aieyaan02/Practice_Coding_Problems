ad98dbf30678e5c1ba09df4b1c0b112978c15899
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-09 22:57:15 -0400

    work

--- Q4.java
+++ Q4.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 public class Q4 {
     public static Double circumference (Double radius){
-        return 2 * Math.PI * radius;
-        
+        return 2 * Math.PI * radius;       
     }
     public static void main (String []args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the radius of the circle: ");
-        double r = scan.nextDouble();
+        double radius = scan.nextDouble();
         System.out.println("The circumference of the circle is: " + circumference(r));
         scan.close();
     }
