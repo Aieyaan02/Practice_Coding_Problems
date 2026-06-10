2171616e6b9b0ee5562a664f1f9e910a3ae1a671
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-10 15:46:45 -0400

    function done

--- Q5.java
+++ Q5.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class Q5 {
-    public static void elegible (int a){
+    public static void elegible (int vote){
+        if(age > 18){
+            System.out.println("Elegible to vote");
+        } else {
+            System.out.println("You are not elegible to vote right now");
+        }
 
     }
     public static void main (String[]args){
