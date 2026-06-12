4d138ddc30f0a964a2560878ff432466ce76a2bc
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-12 01:40:43 -0400

    error

--- Q7.java
+++ Q7.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 public class Q7 {
-    public static void main (String []args){
-        
+    public static void main (String[]args){
+        int positve = 0;
+        int negative = 0;
+        int zero = 0;
+
+        if(positve>0){
+            System.out.println("Positive");
+        } else (negative<0) {
+            System.out.println("Negative");
+
+
+        }
+
     }
     
 }
