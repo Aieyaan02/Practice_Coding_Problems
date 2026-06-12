4f22dc25b06318b4d512123a1531046a03dfc6ec
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-11 23:11:01 -0400

    new scanner

--- Q6.java
+++ Q6.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 public class Q6 {
     public static void infiniteLoop (int a){
+        
 
     }
     public static void main (String[]args){
-
+        Scanner scan = new Scanner(System.in);
+        
     }
     
 }
