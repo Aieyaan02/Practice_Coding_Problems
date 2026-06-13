6edc285baa5d71cdd6d1488b3331d6be4df1d071
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-13 05:15:22 -0400

    delete

--- Q7.java
+++ Q7.java
@@ -3,9 +3,9 @@ public class Q7 {
     public static void main (String[]args){
         int positve = 0;
         int negative = 0;
-        int zero = 0;
+        
 
-       // Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
 
     }
