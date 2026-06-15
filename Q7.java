62e909b2d407d6b7b3423be73ede5d8332ed669c
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-14 21:03:54 -0400

    positive

--- Q7.java
+++ Q7.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class Q7 {
     public static void main (String []args){
+        int positive  = 0;
+        int 
     Scanner scan = new Scanner(System.in);
-    
+
     }
 
 }
