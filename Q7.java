b4a142ddf4de6e3ffc29ee8eaaff1d50bee6880c
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-15 22:02:33 -0400

    s

--- Q7.java
+++ Q7.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Q7 {
     public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        int positive = scan.nextInt();
         
         
     
