11eaaeec7baed77c1b3ffe37e2472f796c59813f
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-15 23:09:28 -0400

    w

--- Q7.java
+++ Q7.java
@@ -3,7 +3,15 @@ public class Q7 {
     public static void main (String[]args){
         Scanner scan = new Scanner(System.in);
         int positive = scan.nextInt();
-        
+        int negative = scan.nextInt();
+        int zero = 0;
+        if(positive > 0){
+            System.out.println("Positive");
+        } else if (negative < 0){
+            System.out.println("Negative");
+        } else {
+            System.out.println(zero);
+        }
         
     
     }
