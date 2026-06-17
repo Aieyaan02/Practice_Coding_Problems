3287aa6e79df1a436c311e0922b6a3978610a053
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-16 23:11:15 -0400

    null

--- Q7.java
+++ Q7.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
 public class Q7 {
     public static void main (String[]args){
-        Scanner scan = new Scanner(System.in);
-        int positive = scan.nextInt();
-        int negative = scan.nextInt();
+        int positive = 0;
+        int negative = 0;
         int zero = 0;
-        if(positive > 0){
-            System.out.println("Positive");
-        } else if (negative < 0){
-            System.out.println("Negative");
-        } else {
-            System.out.println(zero);
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        while(input == 1){
+            System.out.println("Enter a number: ");
+            int n = scan.nextInt();
+            if (n > 0){
+                positive++;
+            } else if (n < 0){
+                negative++;
+            } else {
+                zero++;
+            }
+            System.out.println("Do you want to continue? (1 for yes, 0 for no): ");
+            num = scan.nextInt();
+            scan.close();
         }
-        
+        System.out.println("Number of positive numbers: " + positive);
+        System.out.println("Number of negative numbers: " + negative);
+        System.out.println("Number of zeros: " + zero);
+     }  
     
     }
 
-}
-    
+
 
