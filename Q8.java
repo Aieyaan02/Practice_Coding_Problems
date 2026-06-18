0d72a26341bceadd9bc51932967bdd2bda7fa050
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-17 23:01:05 -0400

    main

--- Q8.java
+++ Q8.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Q8 {
     public static double raisePower (double a, double n){
         return Math.pow(a,n);
-
+    }
+    public static void main(String[]args){
+        
     }
 }
 
