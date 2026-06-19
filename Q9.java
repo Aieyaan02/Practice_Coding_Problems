45128719bf0407e4b19fa5415e4bdfa52d86afbf
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-18 23:23:18 -0400

    nukll

--- Q9.java
+++ Q9.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Q9 {
     public static int commonDivisor (int a, int b){
         int GCD = 1;
-        for (int i = 1, i <= a && i <= b; i++){
-            
+        for (int i = 0, i <= a && i <= b; i++){
+
 
         }
 
