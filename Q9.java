d304b134c69bd6220b422eb212f48c20b34ee5cd
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-18 23:22:40 -0400

    null

--- Q9.java
+++ Q9.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class Q9 {
-    public static CommonDivisor (int a, int b){
-        
+    public static int commonDivisor (int a, int b){
+        int GCD = 1;
+        for (int i = 1, i <= a && i <= b; i++){
+            
+
+        }
+
+        return commonDivisor(a, b);
+
     }
     
 }
