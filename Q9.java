9a33626779950ce4e9308d83cba2037470467582
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-18 23:24:28 -0400

    null

--- Q9.java
+++ Q9.java
@@ -3,6 +3,9 @@ public class Q9 {
     public static int commonDivisor (int a, int b){
         int GCD = 1;
         for (int i = 0, i <= a && i <= b; i++){
+            if (a % i == 0 && b % i == 0){
+                GCD = i;
+            }
 
 
         }
