9e94d45de20e9bb914aef7fdd9e28291187989da
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-22 20:43:25 -0400

    s

--- Q9.java
+++ Q9.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Q9 {
-    public static int commonDivisor (int a, int b){
-        
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();  
+        scan.close();
+        while(num1 != num2){
+            if(num1 > num2){
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+        }
+        System.out.println("The GCD of the two numbers is: " + num2);   
     } 
 }
