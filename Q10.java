cad45e5857bb866c3b076effa296e27a7db8535d
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-23 23:35:46 -0400

    S

--- Q10.java
+++ Q10.java
@@ -1,13 +1,10 @@
 public class Q10 {
     public static void main(String[]args){
-        
-        pattern1(5);
 
+        pattern1(5);
 
         }
     public static void pattern1(int n){
-
-
 
     }
 }
