23dde9e1ccb8209eaf0c6e3ae0b70fe2874a06d0
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-25 03:37:48 -0400

    Q11 pattern row = col

--- Q11.java
+++ Q11.java
@@ -4,7 +4,12 @@ public class Q11 {
 
     }
     static void pattern2(int n){
+        for (int row = 1; row <=n;row++){
+            for (int col = 1; col <= row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
 
+        }
     }
-    
 }
