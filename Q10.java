b2f882cfc4f51d0e3e5a079fb2dce46adfa4c2b4
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-25 03:27:18 -0400

    Q10 pattern sqaure

--- Q10.java
+++ Q10.java
@@ -5,7 +5,12 @@ public class Q10 {
 
         }
     static void pattern1(int n){
-
+        for(int row = 1; row <= n; row++){
+            for (int col = 1; col<=n;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
