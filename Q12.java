e5f1de5c99c34642ba120303038a4068663c8424
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-26 04:15:13 -0400

    S

--- Q12.java
+++ Q12.java
@@ -1,9 +1,19 @@
 public class Q12 {
     public static void main(String[]args){
+        pattern3(5, 4);
 
     }
-    static void pattern3(int n){
-        
+    static void pattern3(int n,int m){
+        for(int row = 1; row <= n;row++){ 
+            for(int col = 1;col <= m;col++){
+                if(row == 1 || row == n-1|| col == 1 || col==m-1){
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+                System.out.println();
+
+            }
+        }
     }
-    
 }
