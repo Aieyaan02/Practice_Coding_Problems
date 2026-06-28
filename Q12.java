ada1d024fb49250bf583cf63af9c37eb35d16a0d
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-28 18:18:23 -0400

    Question 12 hollow square

--- Q12.java
+++ Q12.java
@@ -1,6 +1,6 @@
 public class Q12 {
     public static void main(String[]args){
-        pattern3(5, 5);
+        pattern3(5, 4);
     }
     static void pattern3(int n,int m){
         for(int row = 1;row <= n;row++){ 
@@ -8,7 +8,7 @@ public class Q12 {
                 if(row == 1 || row == n|| col == 1 || col==m){
                     System.out.print(" * ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("   ");
                 }
             }
             System.out.println(); 
