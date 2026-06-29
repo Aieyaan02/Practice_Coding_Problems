ebc6bc43a64c3b09c41a8e4f8a494d5833718a44
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-06-28 20:56:06 -0400

    Q13 inverted triagle

--- Q13.java
+++ Q13.java
@@ -1,9 +1,13 @@
 public class Q13 {
     public static void main (String[]args){
-
+        pattern4(5);
     }
     static void pattern4(int n){
-        
-    }
-    
+        for(int row = n; row >= 1; row--){
+            for(int col = 1; col <= row ; col++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    } 
 }
