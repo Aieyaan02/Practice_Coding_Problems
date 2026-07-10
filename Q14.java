e4c5f7260c8cc0926b4942944fc88829a133e9c7
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-10 04:29:25 -0400

    S

--- Q14.java
+++ Q14.java
@@ -5,7 +5,7 @@ public class Q14 {
     static void pattern5(int n){
         for(int row = 1; row >= n;row++){
             for(int col = 1; col >= n-1;col++){
-                System.out.print("  ");
+                System.out.print("   ");
             }
             for(int str = 1;str >= n;str--){
                 System.out.print(" * ");
