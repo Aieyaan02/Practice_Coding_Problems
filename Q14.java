e1a074fba0e7b0b3eb566cb0b7bfdf364d3e6989
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-07-14 03:03:49 -0400

    S

--- Q14.java
+++ Q14.java
@@ -6,8 +6,7 @@ public class Q14 {
         for(int row = n; row >= 1;row--){
             for(int col = 1; col<row;col++){
                 System.out.print("   ");
-            }
-            
+            }            
             for(int str = 0;str <= n-1;str++){
                 System.out.print(" * ");                               
             }  
