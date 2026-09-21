c162a8678696145e1d9b5713ee4afb9fa5153ea2
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-21 05:39:20 -0400

    Q17

---
+++ Q17.java
@@ -0,0 +1,16 @@
+public class Q17 {
+    public static void main (String []args){
+        SumEven();
+    }
+    static void SumEven(){
+        int sum = 0;
+        for (int i = 1;i <= 10; i++){
+            if(i%2 == 0){
+                sum +=i;
+            System.out.println(sum);
+
+            }
+
+        }
+    }
+}
