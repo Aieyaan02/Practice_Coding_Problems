e17e6a079d7d14dae3d6e366cfed5085bf196414
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-21 05:22:09 -0400

    S

--- Q15.java
+++ Q15.java
@@ -1,12 +1,10 @@
 public class Q15 {
     public static void main (String []args){
         countback();
-
     }
     static void countback(){
         for(int i=10;i>=2;i-=2){
             System.out.println(i);
-
         }
     }
 }
