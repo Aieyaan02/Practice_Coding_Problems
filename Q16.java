af71a27c30fdcc0863b70babda5c30eaa30d83cf
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-21 05:32:24 -0400

    Q16

--- Q16.java
+++ Q16.java
@@ -1,9 +1,12 @@
 public class Q16 {
     public static void main (String[]args){
-
+        calculateSum();
     }
     static void calculateSum(){
-
+        int sum = 0;
+        for(int i = 1;i<=5;i++){
+             sum += i;
+             System.out.println(sum);
+        }
     }
-    
 }
