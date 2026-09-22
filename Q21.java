d1b656b87bdc061d0e9b2ec11b8277568422dbc3
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-22 04:17:54 -0400

    S

--- Q21.java
+++ Q21.java
@@ -1,9 +1,13 @@
 public class Q21 {
     public static void main (String []arrgs){
-        numTringle();
+        numTringle(5);
     }
-    static void numTringle(){
-        
-
+    static void numTringle(int n){
+        for(int row = n;row >=1;row--){
+            for(int col = row;col <=n;col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 }
