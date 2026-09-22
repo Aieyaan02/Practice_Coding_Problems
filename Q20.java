4713c5c1c5244149a4b991820c8036336d248d54
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-22 04:01:34 -0400

    Q20

--- Q20.java
+++ Q20.java
@@ -1,8 +1,8 @@
 public class Q20 {
     public static void main (String []args){
-        Array_Find();
+        findTarget();
     }
-    static void Array_Find(){
+    static void findTarget(){
         int[] num = {4,8,3,7,2};
         int tagert = 7;
         for(int i = 0; i< num.length;i++){
