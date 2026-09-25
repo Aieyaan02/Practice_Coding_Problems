66625e825448281fa00ddc83213a02857d1ff5ab
Aieyaan Yeasin <Aieyaan.yeasin@gmail.com>
2026-09-24 21:39:51 -0400

    S

--- Q21.java
+++ Q21.java
@@ -15,7 +15,7 @@ public class Q21 {
            System.out.println();
            System.out.println();
            System.out.println();
-           System.out.println();
+           
             
         }
     }
